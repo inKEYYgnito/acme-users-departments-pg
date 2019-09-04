d507c3afed1c6bb184a570ea2d771389702df472
Kristina Cayetano <kma.cayetano@gmail.com>
2019-09-03 23:08:23 -0400

    updated bios for users

--- db/db.js
+++ db/db.js
@@ -11,7 +11,12 @@ const departmentsID = {
 const usersID = {
     MOE: uuid.v4(),
     LARRY: uuid.v4(),
-    KATSU: uuid.v4()
+    KATSU: uuid.v4(),
+    KEY: uuid.v4(),
+    JUSTIN: uuid.v4(),
+    CHARM: uuid.v4(),
+    KISTINA: uuid.v4(),
+    DIANE: uuid.v4(),
 }
 
 const SQL = `
@@ -24,10 +29,18 @@ const SQL = `
     CREATE TABLE users (
         id UUID PRIMARY KEY,
         name VARCHAR(255),
+        bio VARCHAR(255) DEFAULT 'User has not yet added their bio...',
         department_id UUID references departments(id)
     );
-    INSERT INTO departments(id, name) values('${departmentsID.HR}', 'HR');
+    ${ Object.keys(departmentsID).map(dept => `INSERT INTO departments(id, name) values('${departmentsID[dept]}', '${dept}');`).join('')}
     INSERT INTO users(id, name, department_id) values ('${usersID.MOE}', 'MOE', '${departmentsID.HR}');
+    INSERT INTO users(id, name, department_id) values ('${usersID.LARRY}', 'LARRY', '${departmentsID.SALES}');
+    INSERT INTO users(id, name, department_id) values ('${usersID.KATSU}', 'KATSU', '${departmentsID.MARKETING}');
+    INSERT INTO users(id, name, department_id) values ('${usersID.KEY}', 'KEY', '${departmentsID.IT}');
+    INSERT INTO users(id, name, department_id) values ('${usersID.JUSTIN}', 'JUSTIN', '${departmentsID.IT}');
+    INSERT INTO users(id, name, department_id) values ('${usersID.CHARM}', 'CHARM', '${departmentsID.IT}');
+    INSERT INTO users(id, name, department_id) values ('${usersID.KISTINA}', 'KISTINA', '${departmentsID.HR}');
+    INSERT INTO users(id, name) values ('${usersID.DIANE}', 'DIANE');
 `
 
 const client = new pg.Client('postgres://localhost/acme_db')
